cdac57584e4d27f5dfc57a4325c49d8f1873a1a8
Sandhya Timalsena <sandhyatimalsena29@gmail.com>
2024-12-14 10:52:13 +0545

    Add project files.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace ExpenseTrackerApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
